45e07f2dc3e63408ebda529e180ef9e3502c2fef
admire93 <khj983@lycos.co.kr>
2009-07-20 22:06:30 +0900

    New => PasswordHash in Model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'sha1'
+
 class User < ActiveRecord::Base
 	has_and_belongs_to_many :teams
 	has_many :diaries
@@ -8,7 +10,34 @@ class User < ActiveRecord::Base
 	validates_presence_of :alias
 	validates_uniqueness_of :alias
 
+	def initialize(data = {})
+	  if data[:password].to_s.empty?
+			data[:password_hash] = ''
+		else
+			data[:password_hash] = PasswordHash.hash(data[:email],data[:password]) 
+			data.delete :password
+		end
+		super(data)
+	end
+	
 	def password
+		return if password_hash.to_s.empty?
+		PasswordHash.new(email, password_hash)
+	end
+	
+end
 
+class PasswordHash	
+	def self.hash(email, password)
+		SHA1.new(SHA1.new(password.reverse).to_s + "\n" + email).to_s.reverse
+	end
+
+	def initialize(email, password_hash)
+		@email = email
+		@password_hash = password_hash
+	end
+	
+	def ==(password)
+		@password_hsh = PasswordHash.hash(@email, password)
 	end
 end
